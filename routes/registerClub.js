e77df7b87ccc82c13cfd1463c1bcf80c20d24b89
Lucas Ference <ference.lucas@gmail.com>
2022-06-03 00:09:04 -0400

    Add club insertion

--- routes/registerClub.js
+++ routes/registerClub.js
@@ -7,30 +7,70 @@ async function registerClub (fastify, options) {
         
         if (!isValidRequest(fastify, request.body)) {
 
+            console.log(request.body.club)
+
             reply.status(400)
             return 'bad request'
         }
 
         const body = request.body
-        const clubsCollection = fastify.mongo.db.collection('clubs')
+        const db = fastify.mongo.db
 
-        const doc = {
-            "clubId": body.clubId
-        }
-
-        const result = await clubsCollection.insertOne(doc)
-
-        console.log(result)
+        upsertClub(db.collection('clubs'), body.club)
 
         reply.send('ok')
     })
 }
 
+async function upsertClub (collection, clubReq) {
+
+    const filter = { clubId: clubReq.clubId }
+    const options = { upsert: true }
+
+    const updateDoc = {
+        $set: {
+            clubName: clubReq.clubName,
+            courses: []
+        }
+    }
+
+    const result = await collection.updateOne(filter, updateDoc, options)
+    console.log(
+      `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+    )
+}
+
 function isValidRequest (fastify, body) {
 
     return body != null
-        && !fastify.isStrEmpty(body.clubId)
-        && body.courses != null
+        && validateClub(fastify, body.club)
+        && validateCourses(fastify, body.club.courses)
+}
+
+function validateClub (fastify, club) {
+
+    return club != null
+        && !fastify.isStrEmpty(club.clubId)
+        && !fastify.isStrEmpty(club.clubName)
+}
+
+function validateCourses (fastify, courses) {
+
+    if (courses == null) {
+        return false
+    }
+
+    for (const c of courses) {
+        
+        const isValidCourse = !fastify.isStrEmpty(c.courseId)
+            && !fastify.isStrEmpty(c.courseName)
+
+        if (!isValidCourse) {
+            return false
+        }
+    }
+
+    return true
 }
 
 export default registerClub
