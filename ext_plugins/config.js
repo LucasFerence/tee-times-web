4208c3c5d4508ca7b1e239789ccadbe6325ab29d
Lucas Ference <ference.lucas@gmail.com>
2022-07-18 20:45:11 -0400

    Finish init react client

--- ext_plugins/config.js
+++
@@ -1,9 +0,0 @@
-import fastifyPlugin from 'fastify-plugin'
-import nodeConfig from 'config'
-
-async function config (fastify, options) {
-
-    fastify.decorate('config', nodeConfig.get('Root'))
-}
-
-export default fastifyPlugin(config)
