aa5e7a8030618fd3ae87deeb237cfbaa6e453b8e
Lucas Ference <ference.lucas@gmail.com>
2022-07-26 21:37:06 -0400

    Refactor course to support forms

--- client/src/views/Course.js
+++ client/src/views/Course.js
@@ -1,47 +1,78 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import axios from 'axios';
+import dayjs from 'dayjs'
 
-class Course extends React.Component {
+import { Button, Select } from '@mantine/core';
+import { DatePicker, TimeInput } from '@mantine/dates';
+import { useForm } from '@mantine/form';
 
-    constructor(props) {
-        super(props);
+function Course(props) {
 
-        this.state = {
-            course: null
-        }
-    }
+    // Load the course, add it to state
+    const [course, setCourse] = useState();
 
-    componentDidMount() {
-        this.refreshStateFromApi()
-    }
+    useEffect(() => {
 
-    componentDidUpdate(prevProps) {
-
-        if (this.props.courseId !== prevProps.courseId) {
-            this.refreshStateFromApi()
-        }
-    }
-
-    refreshStateFromApi() {
-        axios.get(`courses/${this.props.clubId}/${this.props.courseId}`)
+        axios.get(`courses/${props.clubId}/${props.courseId}`)
             .then(res => {
-                const course = res.data;
-                this.setState({ course })
+                setCourse(res.data)
             })
-    }
 
-    render() {
+    }, [props.clubId, props.courseId])
 
-        let courseName = this.state.course != null
-            ? this.state.course.courseName
-            : null
+    // Initialize the form
 
-        return (
-            <div>
-                {courseName}
-            </div>
-        )
-    }
+    const form = useForm({
+
+        initialValues: {
+            numPlayers: '4',
+            teeTimeDate: null,
+            minTime: null,
+            maxTime: null
+        },
+
+        validate: {
+            teeTimeDate: (value) => (value == null ? 'Must select tee time date' : null),
+            minTime: (value) => (value == null ? 'Must select minimum time' : null),
+            maxTime: (value) => (value == null ? 'Must select maxium time' : null)
+        }
+    })
+
+    return (
+        <form onSubmit={form.onSubmit(console.log)}>
+
+            <Select
+                label="Number of players"
+                data={['1','2','3','4']}
+                required
+                {...form.getInputProps('numPlayers')}
+            />
+
+            <DatePicker
+                placeholder="Pick Date"
+                label="Tee time date"
+                required
+                minDate={dayjs(new Date()).toDate()}
+                {...form.getInputProps('teeTimeDate')}
+            />
+
+            <TimeInput
+                label="Earliest Time"
+                format="12"
+                required
+                {...form.getInputProps('minTime')}
+            />
+
+            <TimeInput
+                label="Latest Time"
+                format="12"
+                required
+                {...form.getInputProps('maxTime')}
+            />
+
+            <Button type="submit">Submit</Button>
+        </form>
+    );
 }
 
 export default Course;
