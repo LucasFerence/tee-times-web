a6626f7d5943c6eb2aecadb5b0ba70677445832a
Lucas Ference <ference.lucas@gmail.com>
2022-08-05 18:21:18 -0400

    Implement time booking

--- server-ts/src/routes/api/chronogolf/scheduleTime.ts
+++ server-ts/src/routes/api/chronogolf/scheduleTime.ts
@@ -1,20 +1,11 @@
 import {FastifyInstance} from 'fastify';
 import {Static, Type} from '@sinclair/typebox';
+import {
+  ScheduleDetails,
+  ScheduleDetailsType,
+} from 'src/schema/chronogolf/schedule';
 import {DateTime} from 'luxon';
 import {Job} from 'agenda';
-
-const RequestType = Type.Object({
-  userId: Type.String(),
-  clubId: Type.String(),
-  courseId: Type.String(),
-  date: Type.String({format: 'date-time'}),
-  playerCount: Type.Integer({minimum: 1, maximum: 4}),
-  earliestTime: Type.String({format: 'date-time'}),
-  latestTime: Type.String({format: 'date-time'}),
-  checkout: Type.Optional(Type.Boolean()),
-});
-
-type Request = Static<typeof RequestType>;
 
 const ResponseType = Type.Object({
   executionTime: Type.String({format: 'date-time'}),
@@ -29,18 +20,18 @@ const ErrorType = Type.Object({
 type Error = Static<typeof ErrorType>;
 
 export default async function scheduleTime(server: FastifyInstance) {
-  server.post<{Body: Request; Reply: Response | Error}>(
+  server.post<{Body: ScheduleDetails; Reply: Response | Error}>(
     '/scheduleChronogolf',
     {
       schema: {
-        body: RequestType,
+        body: ScheduleDetailsType,
         response: {
           200: ResponseType,
         },
       },
     },
     async (request, reply) => {
-      const scheduleDetails: Request = request.body;
+      const scheduleDetails: ScheduleDetails = request.body;
 
       const user = await server.getChronogolfUser(scheduleDetails.userId);
       if (user === null) {
@@ -73,8 +64,7 @@ export default async function scheduleTime(server: FastifyInstance) {
 
       agenda.define(taskId, (job: Job) => {
         console.log(`Starting service for task: ${taskId}`);
-        console.log(job.attrs.data);
-        // Book time here
+        server.bookChronogolfTime(scheduleDetails);
       });
 
       const now = DateTime.now();
@@ -87,10 +77,10 @@ export default async function scheduleTime(server: FastifyInstance) {
 
       if (scheduleCutoff > now) {
         console.log(`Scheduling job: ${taskId}`);
-        agenda.schedule(scheduleCutoff.toJSDate(), taskId, scheduleDetails);
+        agenda.schedule(scheduleCutoff.toJSDate(), taskId, undefined);
       } else {
         console.log(`Executing job immediately: ${taskId}`);
-        agenda.now(taskId, scheduleDetails);
+        agenda.now(taskId, undefined);
       }
     }
   );
