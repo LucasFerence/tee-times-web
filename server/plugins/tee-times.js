f7f74d2639d7e2c9eb423c51ec482bfa131c6296
Lucas Ference <ference.lucas@gmail.com>
2022-08-08 20:40:45 -0400

    Remove old server files

--- server/plugins/tee-times.js
+++
@@ -1,8 +0,0 @@
-import fastifyPlugin from 'fastify-plugin'
-
-// Register utilities for executing on tee times
-async function teeTimes (fastify, options) {
-
-}
-
-export default fastifyPlugin(teeTimes)
