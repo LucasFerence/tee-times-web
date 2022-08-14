4e1ada9c9a4b8a398d982a6cbd21733937a1c99e
Lucas Ference <ference.lucas@gmail.com>
2022-08-13 20:57:33 -0400

    Add rough user login/logout

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { MantineProvider } from '@mantine/core';
 
 import Clubs from './views/Clubs';
+import LoginButton from './auth/LoginButton';
+import LogoutButton from './auth/LogoutButton';
+import UserProfile from './user/UserProfile';
 
 class App extends React.Component {
 
   render() {
     return (
       <MantineProvider>
+
+        <LoginButton />
+        <LogoutButton />
+        <UserProfile />
         
         <Clubs />
 
